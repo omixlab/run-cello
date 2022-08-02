e965a046211d9f7608a6720fa76599256a65027e
fredericokremer <fred.s.kremer@hotmail.com>
2022-08-02 10:42:15 -0300

    first!

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="run-cello",
+    version='0.1.0',
+    packages=find_packages(),
+    author="Frederico Schmitt Kremer",
+    author_email="fred.s.kremer@gmail.com",
+    description="Find protein locations in FASTA files using Cello.",
+    keywords="bioinformatics",
+    entry_points = {'console_scripts':[
+        'run-cello   = cello:main'
+        ]},
+    install_requires = [
+        requirement.strip('\n') for requirement in open("requirements.txt")
+    ]
+)
