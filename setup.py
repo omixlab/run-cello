d3fa2a98f21afa4c29b3d2f5502a6c39f97c3006
fredericokremer <fred.s.kremer@hotmail.com>
2022-08-02 11:06:33 -0300

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="run-cello",
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     author="Frederico Schmitt Kremer",
     author_email="fred.s.kremer@gmail.com",
